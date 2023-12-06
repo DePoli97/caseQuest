4866ef0c45ebfd37c57868b05e47e84ec5c73de5
Gianluca <gianluca.maragliano@usi.ch>
2023-12-06 20:52:45 +0100

    Progress

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -3,12 +3,14 @@ import LandingView from "@/views/LandingView.vue";
 import TutorialView from "@/views/TutorialView.vue";
 import IntroductionView from "@/views/IntroductionView.vue";
 import ExperimentView from "@/views/ExperimentView.vue";
+import GoodbyeView from "@/views/GoodbyeView.vue";
 
 const routes = [
     { path: '/', component: LandingView},
     { path: '/introduction', component: IntroductionView},
     { path: '/tutorial', component: TutorialView},
-    { path: '/experiment', component: ExperimentView}
+    { path: '/experiment', component: ExperimentView},
+    { path: '/end', component: GoodbyeView}
 
 ];
 
---
+++ frontend/src/views/GoodbyeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2> The experiment is over </h2>
+<h1>Thank You for Participating!</h1>
+</template>
+
+<style scoped>
+
+</style>
