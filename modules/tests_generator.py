3a53b163a0501637c48909b9d1f5afd295cfc937
Gianluca <gianluca.maragliano@usi.ch>
2023-12-09 13:55:32 +0100

    fixed problem with right answer

--- modules/tests_generator.py
+++ modules/tests_generator.py
@@ -23,25 +23,25 @@ def generate_tests():
     return tests
 
 
-def generate_type_tests(tests, type):
-    camel_tests = []
+def generate_type_tests(tests, case_type):
+    type_tests = []
     for test in tests:
         case_answer_index = test["sentence"].index(test["case_answer"])
-        camel_tests_sentence = []
+        type_tests_sentence = []
         for couple in test["sentence"]:
             words = couple.split(" ")
             text = words[0]
             for i in range(1, len(words)):
-                text += words[i].capitalize() if type == "camel" else '-' + words[i]
-            camel_tests_sentence.append(text)
+                text += words[i].capitalize() if case_type == "camel" else '-' + words[i]
+            type_tests_sentence.append(text)
 
-        camel_test = {
+        type_test = {
             "answer": test["case_answer"],
-            "case_answer": camel_tests_sentence[case_answer_index],
+            "case_answer": type_tests_sentence[case_answer_index],
             "case_type": "camel",
-            "sentence": camel_tests_sentence,
+            "sentence": type_tests_sentence,
             "question_number": test["question_number"],
             "#words": len(test["sentence"]),
         }
-        camel_tests.append(camel_test)
-    return camel_tests
+        type_tests.append(type_test)
+    return type_tests
